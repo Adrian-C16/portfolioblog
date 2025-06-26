dac4a33c3fd69819cf3ee185dc11376a6c46b288
Adrian-C16 <adriancimadevilagarcia@afd.novadixital.es>
2025-06-26 20:33:33 +0200

    postcard importado en home

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,16 +1,16 @@
 import { Link } from "react-router-dom";
 import '../index.css'
 
-function PostCard({post}) {
+function PostCard({ post }) {
     return (
-        <div className="post-card">
-            <h2 className="post-card-tittle">{post.title}</h2>
-            <p className="post-card-meta">
+        <div className='post-card'>
+            <h2 className='post-title'>{post.title}</h2>
+            <p className='post-excerpt'>{post.excerpt}</p>
+            <p className='post-meta'>
                 Publicado por {post.author} el {post.date}
             </p>
-            <p className="post-card-excerpt">{post.excerpt}</p>
-            <Link to = {`/post/${post.id}`} className="post-card-link">
-            Leer más →
+            <Link to={`/post/${post.id}`} className='read-more'>
+                Leer más →
             </Link>
         </div>
     );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,8 @@
 import '../index.css';
-import {Link} from 'react-router-dom';
 import {posts} from '../data/posts.js';
 import HeroIntro from '../components/HeroIntro.jsx';
+import PostCard from '../components/PostCard.jsx';
  
-
-
 function Home() {
     return (
         <div className='home'>
@@ -12,16 +10,7 @@ function Home() {
             <h1 className='home-title'>Últimas Publicaciones</h1>
             <div className='post-list'>
                 {posts.map((post) => (
-                    <div key={post.id} className='post-card'>
-                        <h2 className='post-title'>{post.title}</h2>
-                        <p className='post-excerpt'>{post.excerpt}</p>
-                        <p className='post-meta'>
-                            Publicado por {post.author} el {post.date}
-                        </p>
-                        <Link to = {`/post/${post.id}`} className='read-more'>
-                            Leer más →
-                        </Link>
-                    </div>
+                    <PostCard key={post.id} post={post} />
                 ))}
             </div>
         </div>
